af01dba569330c3d2a778fe620a99fcbc88fc785
Manuel González Gómez <tete.manolo95@gmail.com>
2024-07-09 18:36:31 +0200

    Test Page Model

--- cypress/e2e/todoTest.cy.js
+++ cypress/e2e/todoTest.cy.js
@@ -4,9 +4,9 @@ describe('Test ToDo', () => {
     gestiontarea.complete()
     })
     it(' Comprobar que al marcar tareas el filtro Active funciona correctamente', () => {
-    gestiontarea.active
+    gestiontarea.active()
     })
     it('Agregar dos tareas con el mismo nombre', () => {
-    gestiontarea.doble
+    gestiontarea.doble()
     })
     })
